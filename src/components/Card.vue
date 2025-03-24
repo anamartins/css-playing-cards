8f84e8d5382b45317bdb339bb15005dc64e3482c
Ana Martins <anacarolcm@gmail.com>
2025-03-24 17:20:27 +0100

    Create mid cart part (#3)
    * create mid cart part
    
    * styles for suits and rank

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -7,17 +7,30 @@ defineProps<{ suit: string; rank: string; color: string }>();
 
 <template>
   <div class="card">
-    <Rank :rank="rank" :color="color" />
-    <Suit :suit="suit" />
+    <div class="corner top-left">
+      <Rank :rank="rank" :color="color" />
+      <Suit :suit="suit" />
+    </div>
+
+    <div class="middle">
+      <div class="content" v-if="Number.isInteger(parseInt(rank))">
+        <Suit v-for="i in parseInt(rank)" :suit="suit" :class="rank" />
+      </div>
+    </div>
+    <div class="corner bottom-right">
+      <Rank :rank="rank" :color="color" />
+      <Suit :suit="suit" />
+    </div>
   </div>
 </template>
 
 <style scoped>
 .card {
-  width: 7rem;
-  height: 10rem;
+  display: flex;
+  width: 8rem;
+  height: 11rem;
   margin: 1rem;
-  padding: 0.8rem 1rem;
+  padding: 0.3rem;
   background: #fff;
   border: 1px solid #999;
   border-radius: 5px;
@@ -28,5 +41,30 @@ defineProps<{ suit: string; rank: string; color: string }>();
 
 .card:hover {
   margin: 0.5rem 1rem;
+}
+
+.middle {
+  background: pink;
+  width: 100%;
+  height: calc(100% - 2rem);
+  margin: 1rem 0;
+  display: flex;
+}
+
+.content {
+  width: 100%;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  align-content: space-around;
+}
+.corner {
+  margin-left: 0;
+  margin-right: 10px;
+}
+.bottom-right {
+  transform: rotate(180deg);
+  margin-right: 0;
+  margin-left: 10px;
 }
 </style>
